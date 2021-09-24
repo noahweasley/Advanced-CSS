69001eda3ed33ebca0fb926190292264c74a0b48
Noah <iebenmelu@gmail.com>
2021-09-24 07:57:40 -0700

    finished first practice

---
+++ Borders/script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function $$(selector, context) {
+    context = context || document;
+    var elements = context.querySelectorAll(selector);
+    return Array.prototype.slice.call(elements);
+}
