0085d2d4ad893f0c5810489b8dbc5cad90b75f28
Noah <iebenmelu@gmail.com>
2022-01-25 09:29:19 -0800

    finished fan spin

---
+++ CSS standing fan/script.js
@@ -0,0 +1,14 @@
+// const blades = document.querySelector(".fan-blades");
+
+// const limit = 0.5;
+// let speed = 5;
+
+// let timer = setInterval(() => {
+//   if (speed == 0.5) {
+//       timer = null;
+//       return;
+//   }
+  
+//   speed -= 0.5;
+//   blades.style.setProperty("--fan-speed", `${speed}s`);
+// }, 500);
